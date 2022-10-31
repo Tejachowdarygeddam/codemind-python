6c9073c0d2826e3aacb6ba634419324e89707400
Tejachowdarygeddam <99868930+Tejachowdarygeddam@users.noreply.github.com>
2022-10-31 14:56:51 +0530

    def get_pfs(num):  # n = 10     s=0     for i in range(1, num):         if num%i==0:             s+=i     return s #8 a=int(input()) b=int(input()) if get_pfs(a)==b and get_pfs(b)==a: #get_pfs(10)     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,17 +1,13 @@
+def get_pfs(num):  # n = 10
+    s=0
+    for i in range(1, num):
+        if num%i==0:
+            s+=i
+    return s #8
 a=int(input())
 b=int(input())
-#proper factor sum of a
-pfs_a=0
-for i in range(1,a):
-    if a%i==0:
-        pfs_a+=i
-#proper factor sum of b
-pfs_b=0
-for i in range(1,b):
-    if b%i==0:
-        pfs_b+=i
-
-if pfs_a==b and pfs_b==a:
+if get_pfs(a)==b and get_pfs(b)==a: #get_pfs(10)
     print('Amicable')
 else:
     print('Not Amicable')
+
